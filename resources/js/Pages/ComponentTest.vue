156b4729c05ef8688fb5309c4a6354448a4ac742
greenAmians <music.amiami@gmail.com>
2023-12-02 04:13:21 +0900

    「追加」他のコンポーネントを使ってみる

--- resources/js/Pages/ComponentTest.vue
+++ resources/js/Pages/ComponentTest.vue
@@ -2,11 +2,16 @@
 import GuestLayout from '@/Layouts/GuestLayout.vue'
 import Label from '@/Components/InputLabel.vue'
 import Input from '@/Components/TextInput.vue'
+
+const emitTest = e => console.log(e)
 </script>
 
 <template>
 <GuestLayout>
     <label value="件名">タイトル</label>
-    <Input modelValue="初期値が入ります"></Input>
+    <Input 
+    modelValue="初期値が入ります"
+    @update:modelValue="emitTest"
+    ></Input>
 </GuestLayout>
 </template>
