ec476552a579f453944b75080cbf818ccb76afc5
greenAmians <music.amiami@gmail.com>
2024-01-04 13:55:20 +0900

    エラー

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Models\Customer;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -8,10 +8,13 @@ use App\Http\Controllers\InertiaTestController;
 use App\Http\Controllers\ItemController;
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\PurchaseController;
-use App\Models\Customer;
+use App\Http\Controllers\AnalysisController;
+// use App\Http\Controllers\Api\AnalysisController;
 
 
 // 認証していたら表示
+
+Route::get('analysis', [AnalysisController::class, 'index'])->name('analysis');
 Route::resource('items', ItemController::class)->middleware(['auth','verified']);
 Route::resource('customers',CustomerController::class)->middleware(['auth','verified']);
 Route::resource('purchases',PurchaseController::class)->middleware(['auth','verified']);
