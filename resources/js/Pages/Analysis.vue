f278ad3a5af11dbe1d45784378a88f3d588d7d69
greenAmians <music.amiami@gmail.com>
2024-01-08 19:56:01 +0900

    コンポーネント分離

--- resources/js/Pages/Analysis.vue
+++ resources/js/Pages/Analysis.vue
@@ -3,7 +3,8 @@ import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import { Head } from "@inertiajs/vue3";
 import { reactive, onMounted } from "vue";
 import { getToday } from "@/common";
-import Chart from '@/Components/Chart.vue'
+import Chart from '@/Components/Chart.vue';
+import ResultTable from '@/Components/ResultTable.vue';
 
 onMounted(() => {
     form.startDate = getToday();
@@ -32,6 +33,7 @@ const getData = async () => {
                 data.data = res.data.data;
                 data.labels = res.data.labels;
                 data.totals = res.data.totals;
+                data.type = res.data.type;
                 console.log(res.data);
             });
     } catch (e) {
@@ -62,6 +64,9 @@ const getData = async () => {
 
                             <input type="radio" v-model="form.type" value="perYear">
                             <span class="mr-2">年別</span>
+
+                            <input type="radio" v-model="form.type" value="decile">
+                            <span class="mr-2">デシル分析</span>
                             <br>
                             From:
                             <input
@@ -79,36 +84,8 @@ const getData = async () => {
                         </form>
 
                         <Chart :data="data" />
-
-                        <div v-if="data.data"
-                        class="lg:w-2/3 w-full mx-auto overflow-auto">
-                            <table class="table-auto w-full text-left whitespace-no-wrap">
-                                <thead>
-                                    <tr>
-                                        <th
-                                            class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl"
-                                        >
-                                            年月日
-                                        </th>
-                                        <th
-                                            class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100"
-                                        >
-                                            金額
-                                        </th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr v-for="item in data.data" :key="item.date">
-                                        <td class="border-b-2 border-gray-200 px-4 py-3">
-                                            {{ item.date }}
-                                        </td>
-                                        <td class="border-b-2 border-gray-200 px-4 py-3">
-                                            {{ item.total }}
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
+                        <ResultTable :data="data" />
+                        
                     </div>
                 </div>
             </div>
